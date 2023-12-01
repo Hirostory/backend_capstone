14e27a837423c8a6d1d5c806fcaecf201a5b8c28
hirostory <hiroakisotooka@gmail.com>
2023-12-01 17:09:51 -0500

    connected to MongoDB atlas

--- server.js
+++ server.js
@@ -1,5 +1,5 @@
 // Import Dependencies 
-require("dotenv").config
+require("dotenv").config()
 const express = require("express")
 const methodOverride = require("method-override")
 const app = express()
@@ -9,6 +9,12 @@ const morgan = require("morgan")
 
 const { DATABASE_URL, PORT = 4000 } = process.env || 4000;
 
+mongoose.connect(DATABASE_URL)
+
+mongoose.connection
+    .on("open", () => console.log("You are connected to mongoose"))
+    .on("close", () => console.log("You are disconnected to moongose"))
+    .on("error", (err) => console.log(err))
 
 app.get('/', (req, res) => {
     res.send("Hello World")
